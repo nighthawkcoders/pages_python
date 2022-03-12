bfc1f4aaba7454571582bbe9a7b3031725060750
Sunny  Naidu <sunnynaidu1970@gmail.com>
2022-03-12 08:16:11 -0800

    Submenu for Math functions

--- src/menuy.py
+++ src/menuy.py
@@ -1,7 +1,7 @@
 # menuy.py - function style menu
 # Imports typically listed at top
 # each import enables us to use logic that has been abstracted to other files and folders
-import matrix, swap, carList
+import matrix, swap, carList, mathy
 
 # Menu banner
 border = "=" * 25
@@ -13,7 +13,16 @@ banner = f"\n{border}\nPlease Select An Option\n{border}"
 main_menu = [
     ["Matrix", matrix.driver],
     ["Swap", swap.driver],
-    ["List", carList.driver],
+    ["List", carList.driver]
+]
+
+# Submenu list of [Prompt, Action]
+# Works similarly to main_menu
+sub_menu = [
+    ["Factors", mathy.factors],
+    ["GCD", mathy.gcd],
+    ["LCM", mathy.lcm],
+    ["Primes", mathy.primes],
 ]
 
 
@@ -65,6 +74,14 @@ def menu(banner, options):
     menu(banner, options)  # recursion, start menu over again
 
 
+# def submenu
+# using sub menu list above:
+# sub_menu works similarly to menu()
+def submenu():
+    title = "Function Submenu" + banner
+    menu(title, sub_menu)
+
+
 # def menu
 # using main_menu list:
 # 1. main menu and submenu reference are created [Prompts, Actions]
@@ -72,6 +89,7 @@ def menu(banner, options):
 def driver():
     title = "Function Menu" + banner
     menu_list = main_menu.copy()
+    menu_list.append(["Math", submenu])
     menu(title, menu_list)
 
 

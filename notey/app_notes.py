e452d05f38b2f0fb5022387d5544dd3cc696308d
John Mortensen <jmortensen@powayusd.com>
2022-05-01 20:19:03 -0700

    Update Notes comments for view and control

--- notey/app_notes.py
+++ notey/app_notes.py
@@ -1,5 +1,5 @@
 import markdown
-from flask import Blueprint, render_template,request,redirect,url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from cruddy.query import user_by_id
 from cruddy.model import Notes
@@ -10,6 +10,7 @@ app_notes = Blueprint('notes', __name__,
                       template_folder='templates/notey/',
                       static_folder='static',
                       static_url_path='static')
+
 
 @app_notes.route('/notes')
 @login_required
@@ -23,26 +24,31 @@ def notes():
 
     # if user object is found
     if uo is not None:
-        user = uo.read()                                    # extract user record (Dictionary)
-        for note in uo.notes:                               # loop through each user note
-            note = note.read()                              # extract note record (Dictionary)
+        user = uo.read()  # extract user record (Dictionary)
+        for note in uo.notes:  # loop through each user note
+            note = note.read()  # extract note record (Dictionary)
             note['note'] = markdown.markdown(note['note'])  # convert markdown to html
-            list_notes.append(note)                         # prepare note list for render_template
+            list_notes.append(note)  # prepare note list for render_template
         if list_notes is not None:
             list_notes.reverse()
     # render user and note data in reverse chronological order
     return render_template('notes.html', user=user, notes=list_notes)
 
+
 # Notes create/add
 @app_notes.route('/create/', methods=["POST"])
+@login_required
 def create():
-    """gets data from form and add it to Users table"""
+    """gets data from form and add to Notes table"""
     if request.form:
-        notesObj = Notes(
+        # construct a Notes object
+        note_object = Notes(
             request.form.get("notes"), current_user.userID
         )
-        notesObj.create()
+        # create a record in the Notes table with the Notes object
+        note_object.create()
     return redirect(url_for('notes.notes'))
+
 
 # Preserve redirect after login to go to intended next page
 next_page = None
